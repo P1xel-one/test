1abc7aced6030602931c0e51dd8a55e61d51f55b
P1xel-one <bichixin.m@icloud.com>
2026-05-04 12:39:07 +0500

    Delete demo67 directory

--- demo67/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module org.example.demo67 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens org.example.demo67 to javafx.fxml;
-    exports org.example.demo67;
-}
